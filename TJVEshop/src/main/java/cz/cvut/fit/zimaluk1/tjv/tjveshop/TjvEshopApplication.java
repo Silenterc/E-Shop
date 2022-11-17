a527a8449f476a754a0e46ed1a816e1ea9846372
Lukas Zima <zimaluk1@fit.cvut.cz>
2022-11-17 17:21:03 +0100

    Created the project using Spring Initializer.

---
+++ TJVEshop/src/main/java/cz/cvut/fit/zimaluk1/tjv/tjveshop/TjvEshopApplication.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.zimaluk1.tjv.tjveshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TjvEshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TjvEshopApplication.class, args);
+    }
+
+}
---
+++ TJVEshop/src/test/java/cz/cvut/fit/zimaluk1/tjv/tjveshop/TjvEshopApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.zimaluk1.tjv.tjveshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TjvEshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
