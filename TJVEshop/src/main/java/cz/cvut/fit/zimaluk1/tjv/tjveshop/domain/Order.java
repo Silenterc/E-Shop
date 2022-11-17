64eb341556d0a680f1bd3da629acd83bb640ab09
Lukas Zima <zimaluk1@fit.cvut.cz>
2022-11-17 18:31:37 +0100

    Added an Order class

---
+++ TJVEshop/src/main/java/cz/cvut/fit/zimaluk1/tjv/tjveshop/domain/Order.java
@@ -0,0 +1,44 @@
+package cz.cvut.fit.zimaluk1.tjv.tjveshop.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.Objects;
+
+@Entity
+@Data
+public class Order implements DomainEntity<Long>, Serializable {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private Timestamp time;
+    private String state;
+    @ManyToOne
+    private Customer buyer;
+
+    public Order(Long id, Timestamp time, String state, Customer buyer) {
+        this.id = id;
+        this.time = time;
+        this.state = state;
+        this.buyer = Objects.requireNonNull(buyer);
+    }
+    public Order(){}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Order ord = (Order) o;
+        if(getId() == null){
+            return ord.getId() == null;
+        }
+        return getId().equals(ord.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getId() != null ? getId().hashCode() : 0;
+    }
+}
