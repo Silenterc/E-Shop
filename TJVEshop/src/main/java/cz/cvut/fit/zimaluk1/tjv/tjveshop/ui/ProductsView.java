8c429080571293e31f2c66ce3435182efc9f2596
Lukas Zima <zimaluk1@fit.cvut.cz>
2022-12-17 22:19:47 +0100

    Unsuccessfully tried to implement css so I gave up for now. However I implemented cancelling items from the cart and even cancelling the whole cart altogether. Now it is time to tackle making an actual order :D

--- TJVEshop/src/main/java/cz/cvut/fit/zimaluk1/tjv/tjveshop/ui/ProductsView.java
+++ TJVEshop/src/main/java/cz/cvut/fit/zimaluk1/tjv/tjveshop/ui/ProductsView.java
@@ -4,10 +4,12 @@ import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
@@ -49,20 +51,52 @@ public class ProductsView extends HorizontalLayout {
     private void initShoppingCart() {
         cartGrid.setSizeFull();
         cartGrid.setColumns("name", "price", "amount");
+        Button delete = new Button("Delete");
+        cartGrid.addComponentColumn(prod -> {
+            Button del = new Button(new Icon("lumo", "cross"));
+            del.getElement().setAttribute("aria-label", "Close");
+            //We want to decrement the amount of a Product by one
+            //And return it to the Available Products grid
+            del.addClickListener(e -> {
+                Long id = prod.getId();
+                prod.changeAmount(-1);
+                //Remove it from the cart completely if the amount is now 0
+                if(prod.getAmount() < 1){
+                    cart.remove(id);
+                }
+                //Try to find the same product in our Available products Collection
+                Optional <ProductDto> AvProd = fetchedProds.stream().filter(el -> el.getId().equals(id)).findFirst();
+                //If it is present, I just increment its count, if not then I create it and its amount will be 1
+                AvProd.ifPresentOrElse(productDto -> productDto.changeAmount(1), () -> fetchedProds.add(new ProductDto(prod)));
+                refreshAll();
+            });
+            return del;
+        });
         cartGrid.setItems(cart.values());
         Button order = new Button("Order");
+        Button cancel = new Button("Cancel All");
         order.addThemeVariants(ButtonVariant.LUMO_SUCCESS);
         order.addClickListener(e -> orderCart());
+        cancel.addThemeVariants(ButtonVariant.LUMO_ERROR);
+        cancel.addClickListener(e -> cancelCart());
         H2 h = new H2("My Cart");
-        HorizontalLayout header = new HorizontalLayout(h, order);
+        HorizontalLayout header = new HorizontalLayout(h, order, cancel);
         header.setWidthFull();
         header.expand(h);
         order.setHeightFull();
+        cancel.setHeightFull();
         VerticalLayout cartLayout = new VerticalLayout(header, cartGrid);
         cartLayout.setWidth("50%");
         cartLayout.setHeightFull();
         add(cartLayout);
     }
+
+    private void cancelCart() {
+        cart.clear();
+        fetchProducts();
+        refreshAll();
+    }
+
 
     private void orderCart() {
     }
@@ -70,15 +104,18 @@ public class ProductsView extends HorizontalLayout {
     private void addToCart() {
         products.addItemDoubleClickListener(e -> {
             ProductDto added = e.getItem();
-            if(added.getAmount() > 0){
-                added.changeAmount(-1);
-                Long id = added.getId();
-                if(cart.containsKey(id)){
-                    cart.get(id).changeAmount(1);
-                } else{
-                    cart.put(id, new ProductDto(added));
-                }
+
+            added.changeAmount(-1);
+            if(added.getAmount() < 1){
+                fetchedProds.remove(added);
             }
+            Long id = added.getId();
+            if(cart.containsKey(id)){
+                cart.get(id).changeAmount(1);
+            } else{
+                cart.put(id, new ProductDto(added));
+            }
+
             refreshAll();
 
         });
